0994485bfa58ebcff203ab9a3beb51e7731efe86
user <user@DESKTOP>
2024-06-23 02:16:25 +0300

    Добавьте файлы проекта.

---
+++ Paragon/Paragon.cs
@@ -0,0 +1,35 @@
+﻿using static System.Math;
+
+namespace Paragon
+{
+    public abstract class Area
+    {
+        public double GetArea(double r)
+        {
+            const double pi = PI;
+            return pi*Pow(r, 2);
+        }
+    }
+
+    public class AreaOnSides : Area
+    {
+        double a = 10;
+        double b = 10;
+        double c = 10;
+        public double AreaOnSidesCalc()
+        {
+            double p = (a+b+c​)/2;
+            double s = Sqrt(p*(p-a)*(p-b)*(p-c));
+            return s;
+        }
+    }
+    public class Triangle90 : AreaOnSides
+    {
+        public static string IsTriangle90(double a, double b, double c)
+        {
+            if ((a*a+b*b == c*c) || (a*a+c*c == b*b) || (c*c+b*b == a*a))
+                return "да";
+            else return "нет";
+        }
+    }
+}
---
+++ ParagonRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using Paragon;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int areaRadius = 1;
+AreaOnSides areaOnSides = new AreaOnSides();
+Triangle90 triangle90 = new Triangle90();
+Console.WriteLine("Площадь круга (радиус=" + areaRadius.ToString() + ") " + areaOnSides.GetArea(areaRadius).ToString());
+Console.WriteLine("Площадь треугольника по трем сторонам (10x10x10) = " + areaOnSides.AreaOnSidesCalc().ToString());
+Console.WriteLine("Является ли прямоугольным треугольником (1, 1, 1)?: " + Triangle90.IsTriangle90(1, 1, 1));
+Console.WriteLine("Является ли прямоугольным треугольником (3, 4, 5)?: " + Triangle90.IsTriangle90(3, 4, 5));
+Console.ReadLine();
