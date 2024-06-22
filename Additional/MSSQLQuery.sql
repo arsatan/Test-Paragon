a6a9420b40d15295e4180f4fe9d0e133e1741024
user <user@DESKTOP>
2024-06-23 02:57:45 +0300

    Add SQL query

---
+++ Additional/MSSQLQuery.sql
@@ -0,0 +1,11 @@
+USE [Test-CrossJoin]
+GO
+
+SELECT product_name, category_name FROM [dbo].[products]
+CROSS JOIN [dbo].[categories]
+UNION
+SELECT product_name, '' AS category_name FROM [dbo].[products]
+
+GO
+
+
