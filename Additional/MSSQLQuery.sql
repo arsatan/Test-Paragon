951c836e694e32c33ef8c2f3708bba1c21d6a100
user <user@DESKTOP>
2024-06-23 03:04:12 +0300

    SQL Changed

--- Additional/MSSQLQuery.sql
+++ Additional/MSSQLQuery.sql
@@ -1,11 +1,5 @@
-USE [Test-CrossJoin]
-GO
-
 SELECT product_name, category_name FROM [dbo].[products]
 CROSS JOIN [dbo].[categories]
 UNION
 SELECT product_name, '' AS category_name FROM [dbo].[products]
-
-GO
-
-
+ -- как-то так.
